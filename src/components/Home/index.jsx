019f1c39f176a66942516beedca39afc6dd7d362
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2019-12-03 15:46:35 +0100

    stylowanie ThreeColumns

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+// import HomeHeader from "../HomeHeader"
+import HomeThreeColumns from "../HomeThreeColumns"
+import './style.scss';
+
+const Home = () => {
+    return (
+        <>
+            {/*<HomeHeader />*/}
+            <HomeThreeColumns />
+        </>
+    )
+};
+
+export default Home;
