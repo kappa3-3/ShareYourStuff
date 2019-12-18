c63b60dbc26d074063ed0b218e9f6631141e617e
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2019-12-18 22:50:40 +0100

    walidacja

--- src/commons/validationSchema.js
+++ src/commons/validationSchema.js
@@ -3,27 +3,9 @@ import * as Yup from "yup";
 let terms;
 const validationSchema = Yup.object().shape({
     // step 1 - type of items to donate //
-    // clothing: Yup.string().oneOf(terms),
-    // used_clothing: Yup.string().oneOf(terms),
-    // toys: Yup.string().oneOf(terms),
-    // books: Yup.string().oneOf(terms),
-    // other: Yup.string().oneOf(terms),
     items: Yup.boolean()
         .oneOf([true], 'Must choose at least one'),
-
-    // clothing: Yup.boolean(),
-    // used_clothing: Yup.boolean(),
-    // toys: Yup.boolean(),
-    // books: Yup.boolean(),
-    // other: Yup.boolean(),
-    // checkboxes: Yup.addMethod(
-    //     Yup.boolean, 'methodChooseOne', function () {
-    //         return !!(this.clothing || this.used_clothing || this.toys || this.books || this.other);
-    //     }
-    // .then(Yup.boolean.required('Must choose at least one'))
-    // ),
-
-    // step 3 - location //
+        // step 3 - location //
     location: Yup
         .string
         ()
