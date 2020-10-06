154c69ba7c18a004a83f47780ea60c29af3dcda2
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2020-10-06 17:36:18 +0200

    data retrieved from MongoDB

--- src/components/HomeSupport/index.jsx
+++ src/components/HomeSupport/index.jsx
@@ -1,130 +1,97 @@
-import React, { useEffect, useState } from "react";
-import OrganizationRecord from '../HomeOrganizationRecord'
-import { URL_NONPROFIT, URL_NON_GOV, URL_LOCAL_FUND } from '../../commons/URL';
+import React, { useEffect, useState } from 'react';
+import OrganizationRecord from '../HomeOrganizationRecord';
 import decoration from '../../assets/icons/Decoration.svg';
 import './style.scss';
 
 function HomeSupport() {
   const [type, setType] = useState('nonprofit');
   const [organizations, setOrganizations] = useState([]);
-  const [pageCount, setPageCount] = useState(0);
+  const [pageCount, setPageCount] = useState(3);
   const [page, setPage] = useState(0);
 
-  const handleOrganizationType = (e) => {
-    setType(e.target.dataset.opt);
-    setPage(0);
+  const getFromDb = collection => {
+    fetch('/.netlify/functions/organizations', {
+      method: 'POST',
+      body: collection,
+    }).then(res => res.json())
+      .then(org => {
+        setOrganizations(org.data);
+        setPageCount(Math.ceil(org.data.length / 3));
+        setType(collection);
+        setPage(0);
+      });
   };
 
-  const handlePage = (i) => setPage(i);
-
   useEffect(() => {
-    fetch(URL_NONPROFIT)
-      .then(data => data.json())
-      .then(org => setOrganizations(org))
-      .then(() => setPageCount(Math.ceil(organizations.length / 3)))
-  }, [])
-
-
-  // componentDidUpdate(prevProps, prevState) {
-
-  //   if (this.state.type === 'nonprofit' && prevState.type !== this.state.type) {
-
-  //     fetch(URL_NONPROFIT).then(data => data.json()).then(organizations => this.setState({ organizations },
-  //       () => this.setState({ pageCount: Math.ceil(this.state.organizations.length / 3) })));
-  //   } else if (this.state.type === 'non-gov' && prevState.type !== this.state.type) {
-
-  //     fetch(URL_NON_GOV)
-  //       .then(data => data.json()).then(organizations => this.setState({ organizations },
-  //         () => this.setState({ pageCount: Math.ceil(this.state.organizations.length / 3) })));
-  //   } else if (this.state.type === 'local-fund' && prevState.type !== this.state.type) {
-
-  //     fetch(URL_LOCAL_FUND)
-  //       .then(data => data.json()).then(organizations => this.setState({ organizations },
-  //         () => this.setState({ pageCount: Math.ceil(this.state.organizations.length / 3) })));
-  //   }
-  // };
-
-  // useEffect(() => {
-
-  //   if (type === 'nonprofit') {
-  //     fetch(URL_NONPROFIT)
-  //     .then(data => data.json())
-  //     .then(org => setOrganizations(org)
-  //     .then(() => setPageCount( Math.ceil(organizations.length / 3))))
-  //   } else if (type === 'non-gov') {
-  //     fetch(URL_NON_GOV)
-  //       .then(data => data.json())
-  //       .then(org => setOrganizations(org)
-  //       .then(() => setPageCount( Math.ceil(organizations.length / 3))))
-  //   } else if (type === 'local-fund') {
-
-  //     fetch(URL_LOCAL_FUND)
-  //       .then(data => data.json())
-  //       .then(org => setOrganizations(org)
-  //       .then(() => setPageCount( Math.ceil(organizations.length / 3))))
-  //   }
-  // }, [type]);
+    getFromDb('nonprofit');
+  }, []);
 
   const indexStart = 3 * (page + 1) - 3;
   const indexEnd = 3 * (page + 1);
-  const records = organizations.slice(indexStart, indexEnd).map(record => {
-    return (
-      <>
-        <OrganizationRecord
-          key={record.id}
-          name={record.name}
-          description={record.description}
-          status={record.status}
-        />
-      </>
-    )
-  });
+  const records = organizations.slice(indexStart, indexEnd).map(record => (
+    <OrganizationRecord
+      key={record.name}
+      name={record.name}
+      description={record.description}
+      status={record.status}
+    />
+  ));
 
   const pages = () => {
     if (pageCount !== 1) {
-      return [...Array(pageCount).fill(1)].map((item, i) =>
+      return [...Array(pageCount).fill(type)].map((item, i) => (
         <button
-          key={`home-support-${i}`}
-          onClick={() => handlePage(i)}
+          type="button"
+          key={`${item}-${i}`}
+          onClick={() => setPage(i)}
           className={page === i ? 'active-page' : 'home-support-page'}
-        >{i + 1}</button>);
+        >
+          {i + 1}
+        </button>
+      ));
     }
+    return null;
   };
 
   return (
-    <div id='support-container' className='support-container'>
-      <span className='support-title'>Who are we supporting?</span>
-      <img src={decoration} alt='decoration' />
-      <div className='support-types-wrapper'>
-        <span
+    <div id="support-container" className="support-container">
+      <span className="support-title">Who are we supporting?</span>
+      <img src={decoration} alt="decoration" />
+      <div className="support-types-wrapper">
+        <button
+          type="button"
           className={type === 'nonprofit' ? 'support-active' : 'support-type'}
-          data-opt='nonprofit'
-          onClick={handleOrganizationType}
+          data-opt="nonprofit"
+          onClick={e => getFromDb(e.target.dataset.opt)}
         >
           Nonprofit organizations
-                    </span>
-        <span
-          className={type === 'non-gov' ? 'support-active' : 'support-type'}
-          data-opt='non-gov'
-          onClick={handleOrganizationType}
+        </button>
+        <button
+          type="button"
+          className={type === 'nongovern' ? 'support-active' : 'support-type'}
+          data-opt="nongovern"
+          onClick={e => getFromDb(e.target.dataset.opt)}
         >
           Non-governmental organization
-                    </span>
-        <span
-          className={type === 'local-fund' ? 'support-active' : 'support-type'}
-          data-opt='local-fund'
-          onClick={handleOrganizationType}
+        </button>
+        <button
+          type="button"
+          className={type === 'localfunds' ? 'support-active' : 'support-type'}
+          data-opt="localfunds"
+          onClick={e => getFromDb(e.target.dataset.opt)}
         >
           Local fund-raiser
-                    </span>
+        </button>
       </div>
-      <p className='home-support-description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti earum error explicabo facilis illo
-      impedit libero magnam maiores praesentium, quaerat, quidem rem repellendus sunt totam vitae.
-                    Maiores.</p>
-      <div className='organization-list-wrapper'>
+      <p className="home-support-description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Deleniti earum error explicabo facilis illo impedit libero magnam maiores praesentium,
+        quaerat, quidem rem repellendus sunt totam vitae. Maiores.
+      </p>
+      <div className="organization-list-wrapper">
         {records}
       </div>
-      <div className='support-pages'>
+      <div className="support-pages">
         {pages()}
       </div>
     </div>
