74c81b34cca3b5be81ae5253f1e822e924fe3124
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2020-10-06 18:47:17 +0200

    refactored repetetive code

--- src/components/HomeSimpleSteps/index.jsx
+++ src/components/HomeSimpleSteps/index.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import { connect } from "react-redux";
-
-import { setUserStatus } from "../../actions";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setUserStatus } from '../../actions';
 import decoration from '../../assets/icons/Decoration.svg';
 import shirt from '../../assets/icons/Icon-1.svg';
 import bag from '../../assets/icons/Icon-2.svg';
@@ -10,54 +10,93 @@ import search from '../../assets/icons/Icon-3.svg';
 import transport from '../../assets/icons/Icon-4.svg';
 import './style.scss';
 
-class HomeSimpleSteps extends Component {
-	render() {
-		return (
-			<div id='simple-steps-container' className='simple-steps-container'>
-				<h1 className='text-above-decoration'>Only 4 simple steps</h1>
-				<img src={decoration} alt='' />
-				<div className='simple-steps'>
+function HomeSimpleSteps({ authentication }) {
+  const content = [
+    {
+      id: 'category',
+      title: 'Choose items',
+      src: shirt,
+      desc: 'clothing, toys, tools, etc.',
+    },
+    {
+      id: 'containers',
+      title: 'Pack bags',
+      src: bag,
+      desc: 'use trash bags or paper boxes',
+    },
+    {
+      id: 'places',
+      title: 'Select organization',
+      src: search,
+      desc: 'choose only trusted places',
+    },
+    {
+      id: 'transportation',
+      title: 'Order pick up',
+      src: transport,
+      desc: 'courier comes whenever it suits you',
+    },
 
-					<div className='single-step'>
-						<img className='single-step-icon' src={shirt} alt='' />
-						<h3 className='single-step-header'>Choose items</h3>
-						<hr className='single-step-line' />
-						<span className='single-step-description'>clothing, toys, tools, etc.</span>
-					</div>
-					<div className='single-step'>
-						<img className='single-step-icon' src={bag} alt='' />
-						<h3 className='single-step-header'>Pack items</h3>
-						<hr className='single-step-line' />
-						<span className='single-step-description'>use trash bags or paper boxes</span>
-					</div>
-					<div className='single-step'>
-						<img className='single-step-icon' src={search} alt='' />
-						<h3 className='single-step-header'>Decide who you want to help</h3>
-						<hr className='single-step-line' />
-						<span className='single-step-description'>choose trusted place</span>
-					</div>
-					<div className='single-step'>
-						<img className='single-step-icon' src={transport} alt='' />
-						<h3 className='single-step-header'>Order pick up</h3>
-						<hr className='single-step-line' />
-						<span className='single-step-description'>courier comes whenever it suits you </span>
-					</div>
-				</div>
-				<div className='give-away'>
-					{this.props.authentication
-						? <p><NavLink to="/donate">Give<br />away</NavLink></p>
-						: <p><NavLink to="/login">Give<br />away</NavLink></p>
-					}
-				</div>
-			</div>
-		);
-	}
+  ];
+  return (
+    <div
+      id="simple-steps-container"
+      className="simple-steps-container"
+    >
+      <h1 className="text-above-decoration">
+        Only 4 simple steps
+      </h1>
+      <img src={decoration} alt="" />
+      <div className="simple-steps">
+        {content.map(({
+          id, src, desc, title,
+        }) => (
+          <div className="single-step" key={id}>
+            <img
+              className="single-step-icon"
+              src={src}
+              alt=""
+            />
+            <h3 className="single-step-header">{title}</h3>
+            <hr className="single-step-line" />
+            <span className="single-step-description">
+              {desc}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="give-away">
+        {authentication
+          ? (
+            <p>
+              <NavLink to="/donate">
+                Give
+                <br />
+                away
+              </NavLink>
+            </p>
+          ) : (
+            <p>
+              <NavLink to="/login">
+                Give
+                <br />
+                away
+              </NavLink>
+            </p>
+          )}
+      </div>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
-	return {
-		authentication: state.authentication
-	}
+  return {
+    authentication: state.authentication,
+  };
 }
+
+HomeSimpleSteps.propTypes = {
+  authentication: PropTypes.bool.isRequired,
+};
 
 export default connect(mapStateToProps, { setUserStatus })(HomeSimpleSteps);
